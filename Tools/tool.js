548a06e5324424eff2ce4f86d66566664530371b
Maya <maeva.berthail@student.adatechschool.fr>
2024-12-09 16:49:40 +0100

    change pc

---
+++ Tools/tool.js
@@ -0,0 +1,18 @@
+// function which does the same but with existing
+// elem. params : name of the selector as str +
+// data to insert in as a str
+
+function insertElemHTML(selector, data) {
+	if (!selector || !data)
+		return (-1);
+	let nbInserted
+	while (let count)
+}
+
+// function that apply one function to several 
+// event listeners. params : a list of events
+// to apply the callback to + the callback.
+
+// function applyOnEvents(events_list, callback) {
+
+// }
